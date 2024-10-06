0400f4688cb993b9b4b4512e049413c288d79d47
Aqothy <aq0335@gmail.com>
2024-10-06 13:46:35 -0400

    update

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-service = Service('/usr/bin/chromedriver')
+# service = Service('/usr/bin/chromedriver')
 
-with webdriver.Chrome(service=service, options=chrome_options) as d:
+with webdriver.Chrome(options=chrome_options) as d:
     d.get("https://github.com/Ouckah/Summer2025-Internships")
     d.maximize_window()
     cur_jobs = d.find_elements(By.CSS_SELECTOR, value="table")
@@ -34,24 +34,25 @@ with webdriver.Chrome(service=service, options=chrome_options) as d:
     with open("jobs.txt", "w") as f:
         f.write(str(len(cur_jobs)))
 
-    if len(cur_jobs) + 1 > prev_jobs_count:
-        new_jobs_count = len(cur_jobs) + 1 - prev_jobs_count
+    if len(cur_jobs) > prev_jobs_count:
+        new_jobs_count = len(cur_jobs) - prev_jobs_count
         new_jobs = cur_jobs[0:new_jobs_count]
 
         content = ""
         for j in new_jobs:
+            print(j)
             for t in j.text.split("\n"):
                 content += t + "\n"
 
-        msg = MIMEMultipart()
-        msg['From'] = email
-        msg['To'] = email
-        msg['Subject'] = f"{new_jobs_count} new jobs posted!"
-        msg.attach(MIMEText(content, 'plain', 'utf-8'))
+        # msg = MIMEMultipart()
+        # msg['From'] = email
+        # msg['To'] = email
+        # msg['Subject'] = f"{new_jobs_count} new jobs posted!"
+        # msg.attach(MIMEText(content, 'plain', 'utf-8'))
 
-        with SMTP("smtp.gmail.com") as c:
-            c.starttls()
-            c.login(user=email, password=password)
-            c.sendmail(from_addr=email,
-                       to_addrs=email, msg=msg.as_string())
+        # with SMTP("smtp.gmail.com") as c:
+        #     c.starttls()
+        #     c.login(user=email, password=password)
+        #     c.sendmail(from_addr=email,
+        #                to_addrs=email, msg=msg.as_string())
         
